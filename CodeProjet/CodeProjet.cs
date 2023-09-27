03a21f01ddb9784820694a4ac3966826b9a640b2
Mondir Jaatar <Mondir.Jaatar@Axeciel.fr>
2023-09-27 08:10:51 +0200

    a comment

--- CodeProjet/CodeProjet.cs
+++ CodeProjet/CodeProjet.cs
@@ -4,6 +4,10 @@ public record CodeProjet
 {
     public string Radical { get; set; }
     public int Chrono { get; set; }
+    /// <summary>
+    /// Saved == true : already saved in data file
+    /// Saved == false : must be saved in data file
+    /// </summary>
     public bool Saved { get; set; }
 
     public CodeProjet(string radical, int chrono, bool saved)
